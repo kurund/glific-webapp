499cf0922a2958b856304f0f0a6eeba59cab2b04
Vaibhav Rathore <vaibhav@coloredcow.com>
2020-05-28 22:37:05 +0530

    formatting

--- src/store/actions/tag.ts
+++ src/store/actions/tag.ts
@@ -1,22 +1,22 @@
 import { TagAction, TagActions, Tag } from "../../model/";
 
 export function addTag(tag: Tag): TagAction {
-	return {
-		type: TagActions.ADD_TAG,
-		payload: tag,
-	};
+  return {
+    type: TagActions.ADD_TAG,
+    payload: tag,
+  };
 }
 
 export function editTag(tag: Tag): TagAction {
-	return {
-		type: TagActions.EDIT_TAG,
-		payload: tag,
-	};
+  return {
+    type: TagActions.EDIT_TAG,
+    payload: tag,
+  };
 }
 
 export function deleteTag(tagId: number): TagAction {
-	return {
-		type: TagActions.DELETE_TAG,
-		payload: tagId,
-	};
+  return {
+    type: TagActions.DELETE_TAG,
+    payload: tagId,
+  };
 }
--- src/store/reducers/tag.ts
+++ src/store/reducers/tag.ts
@@ -2,19 +2,19 @@ import { TagAction, TagActions, Tag } from "../../model";
 import createReducer from "./createReducer";
 
 export const tagList = createReducer<Tag[]>([], {
-	[TagActions.ADD_TAG](state: Tag[], action: TagAction) {
-		return [...state, action.payload];
-	},
-	[TagActions.EDIT_TAG](state: Tag[], action: TagAction) {
-		return state.map((tag) => {
-			if (tag.id == action.payload.id) {
-				return action.payload;
-			}
-			return tag;
-		});
-	},
-	[TagActions.DELETE_TAG](state: Tag[], action: TagAction) {
-		// remove tag with the given id
-		return state.filter((t) => t.id !== action.payload);
-	},
+  [TagActions.ADD_TAG](state: Tag[], action: TagAction) {
+    return [...state, action.payload];
+  },
+  [TagActions.EDIT_TAG](state: Tag[], action: TagAction) {
+    return state.map((tag) => {
+      if (tag.id == action.payload.id) {
+        return action.payload;
+      }
+      return tag;
+    });
+  },
+  [TagActions.DELETE_TAG](state: Tag[], action: TagAction) {
+    // remove tag with the given id
+    return state.filter((t) => t.id !== action.payload);
+  },
 });
