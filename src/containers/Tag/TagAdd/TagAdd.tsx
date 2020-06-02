7bfe8b8310805dd108bf2e8829708ae6e4b93e22
Vaibhav Rathore <vaibhav@coloredcow.com>
2020-06-02 12:18:29 +0530

    tag init

--- src/containers/Tag/TagAdd/TagAdd.tsx
+++ src/containers/Tag/TagAdd/TagAdd.tsx
@@ -10,122 +10,122 @@ import { RootState } from "../../../store/reducers";
 import { Tag } from "../../../model";
 
 interface TagAddProps {
-	match: any;
+  match: any;
 }
 
 export const TagAdd: React.SFC<TagAddProps> = (props: TagAddProps) => {
-	const tagList = useSelector((state: RootState) => state.tagList);
-	const tagId = props.match ? props.match.params.id : null;
-	const tag = tagId ? tagList.find((tag) => tag.id === Number(tagId)) : null;
+  const tagList = useSelector((state: RootState) => state.tagList);
+  const tagId = props.match ? props.match.params.id : null;
+  const tag = tagId ? tagList.find((tag) => tag.id === Number(tagId)) : null;
 
-	const [name, setName] = useState(tag ? tag.name : "");
-	const [description, setDescription] = useState(tag ? tag.description : "");
-	const [isActive, setIsActive] = useState(tag ? tag.is_active : false);
-	const [isReserved, setIsReserved] = useState(tag ? tag.is_reserved : false);
-	const [languageId, setLanguageId] = useState(tag ? tag.language_id : "");
-	const [parentId, setParentId] = useState(tag ? tag.parent_id : "");
-	const [formSubmitted, setFormSubmitted] = useState(false);
+  const [label, setLabel] = useState(tag ? tag.label : "");
+  const [description, setDescription] = useState(tag ? tag.description : "");
+  const [isActive, setIsActive] = useState(tag ? tag.is_active : false);
+  const [isReserved, setIsReserved] = useState(tag ? tag.is_reserved : false);
+  const [languageId, setLanguageId] = useState(tag ? tag.language_id : "");
+  const [parentId, setParentId] = useState(tag ? tag.parent_id : "");
+  const [formSubmitted, setFormSubmitted] = useState(false);
 
-	const tagActions = useActions(TagActions);
+  const tagActions = useActions(TagActions);
 
-	const saveHandler = () => {
-		const payload: Tag = {
-			id: tag ? tagId : Math.floor(Math.random() * Math.floor(100)),
-			name: name,
-			description: description,
-			is_active: isActive,
-			is_reserved: isReserved,
-			language_id: Number(languageId),
-			parent_id: Number(parentId),
-		};
+  const saveHandler = () => {
+    const payload: Tag = {
+      id: tag ? tagId : Math.floor(Math.random() * Math.floor(100)),
+      label: label,
+      description: description,
+      is_active: isActive,
+      is_reserved: isReserved,
+      language_id: Number(languageId),
+      parent_id: Number(parentId),
+    };
 
-		if (tag) {
-			tagActions.editTag(payload);
-		} else {
-			tagActions.addTag(payload);
-		}
+    if (tag) {
+      tagActions.editTag(payload);
+    } else {
+      tagActions.addTag(payload);
+    }
 
-		setFormSubmitted(true);
-	};
+    setFormSubmitted(true);
+  };
 
-	const cancelHandler = () => {
-		setFormSubmitted(true);
-	};
+  const cancelHandler = () => {
+    setFormSubmitted(true);
+  };
 
-	if (formSubmitted) {
-		return <Redirect to="/tag" />;
-	}
+  if (formSubmitted) {
+    return <Redirect to="/tag" />;
+  }
 
-	let form = (
-		<React.Fragment>
-			<div className={styles.Input}>
-				<label className={styles.Label}>Name</label>
-				<input
-					type="text"
-					name="name"
-					value={name}
-					onChange={(event) => setName(event?.target.value)}
-				/>
-			</div>
-			<div className={styles.Input}>
-				<label className={styles.Label}>Description</label>
-				<input
-					type="text"
-					name="description"
-					value={description}
-					onChange={(event) => setDescription(event?.target.value)}
-				/>
-			</div>
-			<div className={styles.Input}>
-				<label className={styles.Label}>Is Active?</label>
-				<input
-					type="checkbox"
-					name="is_active"
-					checked={isActive}
-					onChange={(event) => setIsActive(event?.target.checked)}
-				/>
-			</div>
-			<div className={styles.Input}>
-				<label className={styles.Label}>Is Reserved?</label>
-				<input
-					type="checkbox"
-					name="is_reserved"
-					checked={isReserved}
-					onChange={(event) => setIsReserved(event?.target.checked)}
-				/>
-			</div>
-			<div className={styles.Input}>
-				<label className={styles.Label}>Language</label>
-				<input
-					type="number"
-					name="language_id"
-					value={languageId}
-					onChange={(event) => setLanguageId(event?.target.value)}
-				/>
-			</div>
-			<div className={styles.Input}>
-				<label className={styles.Label}>Parent</label>
-				<input
-					type="number"
-					name="parent_id"
-					value={parentId}
-					onChange={(event) => setParentId(event?.target.value)}
-				/>
-			</div>
-			<ButtonElement color="primary" onClick={saveHandler}>
-				Save
+  let form = (
+    <React.Fragment>
+      <div className={styles.Input}>
+        <label className={styles.Label}>Label</label>
+        <input
+          type="text"
+          name="label"
+          value={label}
+          onChange={(event) => setLabel(event?.target.value)}
+        />
+      </div>
+      <div className={styles.Input}>
+        <label className={styles.Label}>Description</label>
+        <input
+          type="text"
+          name="description"
+          value={description}
+          onChange={(event) => setDescription(event?.target.value)}
+        />
+      </div>
+      <div className={styles.Input}>
+        <label className={styles.Label}>Is Active?</label>
+        <input
+          type="checkbox"
+          name="is_active"
+          checked={isActive}
+          onChange={(event) => setIsActive(event?.target.checked)}
+        />
+      </div>
+      <div className={styles.Input}>
+        <label className={styles.Label}>Is Reserved?</label>
+        <input
+          type="checkbox"
+          name="is_reserved"
+          checked={isReserved}
+          onChange={(event) => setIsReserved(event?.target.checked)}
+        />
+      </div>
+      <div className={styles.Input}>
+        <label className={styles.Label}>Language</label>
+        <input
+          type="number"
+          name="language_id"
+          value={languageId}
+          onChange={(event) => setLanguageId(event?.target.value)}
+        />
+      </div>
+      <div className={styles.Input}>
+        <label className={styles.Label}>Parent</label>
+        <input
+          type="number"
+          name="parent_id"
+          value={parentId}
+          onChange={(event) => setParentId(event?.target.value)}
+        />
+      </div>
+      <ButtonElement color="primary" onClick={saveHandler}>
+        Save
 			</ButtonElement>
 			&nbsp;
-			<ButtonElement color="secondary" onClick={cancelHandler}>
-				Cancel
+      <ButtonElement color="secondary" onClick={cancelHandler}>
+        Cancel
 			</ButtonElement>
-		</React.Fragment>
-	);
+    </React.Fragment>
+  );
 
-	return (
-		<div className={styles.TagAdd}>
-			<h4>{tag ? "Edit tag information" : "Enter tag information"}</h4>
-			{form}
-		</div>
-	);
+  return (
+    <div className={styles.TagAdd}>
+      <h4>{tag ? "Edit tag information" : "Enter tag information"}</h4>
+      {form}
+    </div>
+  );
 };
--- src/containers/Tag/TagList/TagList.tsx
+++ src/containers/Tag/TagList/TagList.tsx
@@ -1,11 +1,11 @@
 import {
-	IconButton,
-	Paper,
-	Table,
-	TableBody,
-	TableCell,
-	TableHead,
-	TableRow,
+  IconButton,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
 } from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
@@ -18,59 +18,75 @@ import { RootState } from "../../../store/reducers";
 import styles from "./TagList.module.css";
 import { history } from "../../../configureStore";
 import { makeStyles } from "@material-ui/styles";
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 
-export interface TagListProps {}
+
+export interface TagListProps { }
 
 export const TagList: React.SFC<TagListProps> = () => {
-	const tagList = useSelector((state: RootState) => state.tagList);
-	const tagActions = useActions(TagActions);
-	const classes = useStyles();
+  const tagActions = useActions(TagActions);
+  const classes = useStyles();
+  const TAG_QUERY = gql`
+    {
+      tags {
+        id
+        label
+        description
+      }
+    }
+  `;
+  const { loading, error, data } = useQuery(TAG_QUERY);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+  const tagList = data.tags;
+  console.log(tagList);
 
-	return (
-		<Paper className={styles.Paper}>
-			<Table className={styles.Table}>
-				<TableHead>
-					<TableRow>
-						<TableCell padding="default">Name</TableCell>
-						<TableCell padding="default">Description</TableCell>
-						<TableCell padding="default"></TableCell>
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{tagList.map((n: Tag) => {
-						return (
-							<TableRow key={n.id} hover>
-								<TableCell padding="default">{n.name}</TableCell>
-								<TableCell padding="default">{n.description}</TableCell>
-								<TableCell padding="default">
-									<IconButton
-										aria-label="Edit"
-										color="default"
-										onClick={() => history.push(`/tag/${n.id}/edit`)}
-										className={classes.noVerticalPadding}
-									>
-										<EditIcon />
-									</IconButton>
-									<IconButton
-										aria-label="Delete"
-										color="default"
-										onClick={() => tagActions.deleteTag(n.id)}
-										className={classes.noVerticalPadding}
-									>
-										<DeleteIcon />
-									</IconButton>
-								</TableCell>
-							</TableRow>
-						);
-					})}
-				</TableBody>
-			</Table>
-		</Paper>
-	);
+  return (
+    <Paper className={styles.Paper}>
+      <Table className={styles.Table}>
+        <TableHead>
+          <TableRow>
+            <TableCell padding="default">Name</TableCell>
+            <TableCell padding="default">Description</TableCell>
+            <TableCell padding="default"></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {tagList.map((n: Tag) => {
+            return (
+              <TableRow key={n.id} hover>
+                <TableCell padding="default">{n.label}</TableCell>
+                <TableCell padding="default">{n.description}</TableCell>
+                <TableCell padding="default">
+                  <IconButton
+                    aria-label="Edit"
+                    color="default"
+                    onClick={() => history.push(`/tag/${n.id}/edit`)}
+                    className={classes.noVerticalPadding}
+                  >
+                    <EditIcon />
+                  </IconButton>
+                  <IconButton
+                    aria-label="Delete"
+                    color="default"
+                    onClick={() => tagActions.deleteTag(n.id)}
+                    className={classes.noVerticalPadding}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
 };
 
 const useStyles = makeStyles(() => ({
-	noVerticalPadding: {
-		padding: "0px 12px",
-	},
+  noVerticalPadding: {
+    padding: "0px 12px",
+  },
 }));
