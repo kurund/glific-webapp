75d53d74bf6a150ad80c1d4a1b4078e2de9bcfce
Vaibhav Rathore <vaibhav@coloredcow.com>
2020-06-04 16:12:19 +0530

    Working tag form

--- src/containers/Tag/TagAdd/TagAdd.tsx
+++ src/containers/Tag/TagAdd/TagAdd.tsx
@@ -11,7 +11,7 @@ import Formsy from "formsy-react";
 import TextFieldElement from "../../../components/UI/TextField/TextFieldElement";
 
 interface TagAddProps {
-  match: any;
+	match: any;
 }
 
 const GET_LANGUAGES = gql`
@@ -90,165 +90,173 @@ const UPDATE_TAG = gql`
 `;
 
 export const TagAdd: React.SFC<TagAddProps> = (props: TagAddProps) => {
-  const tagId = props.match.params.id ? props.match.params.id : false;
-  const { loading, error, data } = useQuery(GET_TAG, {
-    variables: { id: tagId },
-    skip: !tagId,
-  });
-  const [updateTag] = useMutation(UPDATE_TAG);
+	const tagId = props.match.params.id ? props.match.params.id : false;
+	const { loading, error, data } = useQuery(GET_TAG, {
+		variables: { id: tagId },
+		skip: !tagId,
+	});
+	const [updateTag] = useMutation(UPDATE_TAG);
 
-  const [label, setLabel] = useState("");
-  const [description, setDescription] = useState("");
-  const [isActive, setIsActive] = useState(false);
-  const [isReserved, setIsReserved] = useState(false);
-  const [languageId, setLanguageId] = useState("1");
-  const [parentId, setParentId] = useState("");
-  const [formSubmitted, setFormSubmitted] = useState(false);
-  const languages = useQuery(GET_LANGUAGES);
-  const [createTag] = useMutation(CREATE_TAG, {
-    update(cache, { data: { createTag } }) {
-      const todos: any = cache.readQuery({ query: GET_TAGS });
-      cache.writeQuery({
-        query: GET_TAGS,
-        data: { tags: todos.tags.concat(createTag.tag) },
-      });
-    },
-  });
+	const [label, setLabel] = useState("");
+	const [description, setDescription] = useState("");
+	const [isActive, setIsActive] = useState(false);
+	const [isReserved, setIsReserved] = useState(false);
+	const [languageId, setLanguageId] = useState(1);
+	const [parentId, setParentId] = useState("");
+	const [formSubmitted, setFormSubmitted] = useState(false);
+	const languages = useQuery(GET_LANGUAGES);
+	const [createTag] = useMutation(CREATE_TAG, {
+		update(cache, { data: { createTag } }) {
+			const todos: any = cache.readQuery({ query: GET_TAGS });
 
-  let tag: any = null;
+			cache.writeQuery({
+				query: GET_TAGS,
+				data: { tags: todos.tags.concat(createTag.tag) },
+			});
+		},
+	});
 
-  useEffect(() => {
-    if (tagId && data) {
-      tag = tagId ? data.tag.tag : null;
-      setLabel(tag.label);
-      setDescription(tag.description);
-      setIsActive(tag.isActive);
-      setIsReserved(tag.isReserved);
-      setLanguageId(tag.language.id);
-      setParentId(tag.parent_id);
-    }
-  }, [data]);
+	let tag: any = null;
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+	useEffect(() => {
+		if (tagId && data) {
+			tag = tagId ? data.tag.tag : null;
+			setLabel(tag.label);
+			setDescription(tag.description);
+			setIsActive(tag.isActive);
+			setIsReserved(tag.isReserved);
+			setLanguageId(tag.language.id);
+			setParentId(tag.parent_id);
+		}
+	}, [data]);
 
-  const saveHandler = () => {
-    const payload = {
-      label: label,
-      description: description,
-      is_active: isActive,
-      is_reserved: isReserved,
-      language_id: Number(languageId),
-    };
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error :(</p>;
 
-    if (tagId) {
-      updateTag({
-        variables: {
-          id: tagId,
-          input: payload,
-        },
-      });
-    } else {
-      createTag({
-        variables: {
-          input: payload,
-        },
-      });
-    }
-    setFormSubmitted(true);
-  }
+	const saveHandler = () => {
+		const payload = {
+			label: label,
+			description: description,
+			isActive: true,
+			isReserved: true,
+			languageId: Number(languageId),
+		};
 
-  const cancelHandler = () => {
-    setFormSubmitted(true);
-  };
+		if (tagId) {
+			updateTag({
+				variables: {
+					id: tagId,
+					input: payload,
+				},
+			});
+		} else {
+			createTag({
+				variables: {
+					input: payload,
+				},
+			});
+		}
+		setFormSubmitted(true);
+	};
 
-  if (formSubmitted) {
-    return <Redirect to="/tag" />;
-  }
+	const setValues = (value: any) => {
+		setLabel(value.label);
+		setDescription(value.description);
+		setIsActive(value.isActive);
+		setIsReserved(value.isReserved);
+		setLanguageId(value.language);
+	};
+	const cancelHandler = () => {
+		setFormSubmitted(true);
+	};
 
-  const languageOptions = languages.data
-    ? languages.data.languages.map((language: any) => {
-      return (
-        <option value={language.id} key={language.id}>
-          {language.label}
-        </option>
-      );
-    })
-    : null;
+	if (formSubmitted) {
+		return <Redirect to="/tag" />;
+	}
 
+	const languageOptions = languages.data
+		? languages.data.languages.map((language: any) => {
+				return (
+					<option value={language.id} key={language.id}>
+						{language.label}
+					</option>
+				);
+		  })
+		: null;
 
-  // Assign the names for the different form questions.
-  let textEntries: { [text: string]: string } = {
-    Label: tag ? tag.label : "",
-    Description: tag ? tag.description : "",
-  };
-  let checkEntries: { [text: string]: boolean } = {
-    "Is Active?": tag ? tag.is_active : false,
-    "Is Reserved?": tag ? tag.is_reserved : false,
-  };
-  let numEntries: { [text: string]: number | string } = {
-    Language: tag ? +tag.language_id : "",
-    Parent: tag ? +tag.parent_id : "",
-  };
+	// Assign the names for the different form questions.
 
-  let textCards = Object.keys(textEntries).map((entryName, i) => {
-    return (
-      <div className={styles.Input} key={i}>
-        <label className={styles.Label}>{entryName}</label>
-        <TextFieldElement
-          value={textEntries[entryName]}
-          name={entryName}
-          type="text"
-          validations="isWords"
-          validationError="Invalid input."
-          required
-        />
-      </div>
-    );
-  });
+	let textEntries: { [text: string]: string } = {
+		label: label,
+		description: description,
+	};
+	let checkEntries: { [text: string]: boolean } = {
+		isActive: isActive,
+		isReserved: isReserved,
+	};
+	let numEntries: { [text: string]: number | string } = {
+		language: languageId,
+		Parent: tag ? +tag.parent_id : "",
+	};
 
-  let checkCards = Object.keys(checkEntries).map((entryName, i) => {
-    return (
-      <div className={styles.Input} key={i}>
-        <label className={styles.Label}>{entryName}</label>
-        <CheckboxElement value={checkEntries[entryName]} name={entryName} />
-      </div>
-    );
-  });
+	let textCards = Object.keys(textEntries).map((entryName, i) => {
+		return (
+			<div className={styles.Input} key={i}>
+				<label className={styles.Label}>{entryName}</label>
+				<TextFieldElement
+					value={textEntries[entryName]}
+					name={entryName}
+					type="text"
+					validations="isWords"
+					validationError="Invalid input."
+					required
+				/>
+			</div>
+		);
+	});
 
-  let numCards = Object.keys(numEntries).map((entryName, i) => {
-    return (
-      <div className={styles.Input} key={i}>
-        <label className={styles.Label}>{entryName}</label>
-        <TextFieldElement
-          value={numEntries[entryName]}
-          name={entryName}
-          type="number"
-          validations="isNumeric,isExisty"
-          validationError="Invalid input."
-          required
-        />
-      </div>
-    );
-  });
+	let checkCards = Object.keys(checkEntries).map((entryName, i) => {
+		return (
+			<div className={styles.Input} key={i}>
+				<label className={styles.Label}>{entryName}</label>
+				<CheckboxElement value={checkEntries[entryName]} name={entryName} />
+			</div>
+		);
+	});
 
-  return (
-    <div className={styles.TagAdd}>
-      <h4>{tagId ? "Edit tag information" : "Enter tag information"}</h4>
-      <Formsy onValidSubmit={saveHandler}>
-        {textCards}
-        {checkCards}
-        {numCards}
-        <div className={styles.Buttons}>
-          <ButtonElement type="submit" color="primary">
-            Submit
+	let numCards = Object.keys(numEntries).map((entryName, i) => {
+		return (
+			<div className={styles.Input} key={i}>
+				<label className={styles.Label}>{entryName}</label>
+				<TextFieldElement
+					value={numEntries[entryName]}
+					name={entryName}
+					type="number"
+					validations="isNumeric,isExisty"
+					validationError="Invalid input."
+					required
+				/>
+			</div>
+		);
+	});
+
+	return (
+		<div className={styles.TagAdd}>
+			<h4>{tagId ? "Edit tag information" : "Enter tag information"}</h4>
+			<Formsy onValidSubmit={saveHandler} onChange={setValues}>
+				{textCards}
+				{checkCards}
+				{numCards}
+				<div className={styles.Buttons}>
+					<ButtonElement type="submit" color="primary">
+						Submit
 					</ButtonElement>
 					&nbsp;
 					<ButtonElement color="default" onClick={cancelHandler}>
-            Cancel
+						Cancel
 					</ButtonElement>
-        </div>
-      </Formsy>
-    </div>
-  );
+				</div>
+			</Formsy>
+		</div>
+	);
 };
--- src/containers/Tag/TagList/TagList.tsx
+++ src/containers/Tag/TagList/TagList.tsx
@@ -1,11 +1,11 @@
 import {
-  IconButton,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
+	IconButton,
+	Paper,
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableRow,
 } from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
@@ -17,7 +17,7 @@ import { makeStyles } from "@material-ui/styles";
 import { useQuery, useMutation } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
 
-export interface TagListProps { }
+export interface TagListProps {}
 
 const GET_TAGS = gql`
 	{
@@ -41,74 +41,74 @@ const DELETE_TAG = gql`
 `;
 
 export const TagList: React.SFC<TagListProps> = () => {
-  const classes = useStyles();
-  let deleteId: number = 0;
-  const { loading, error, data } = useQuery(GET_TAGS);
-  const [deleteTag] = useMutation(DELETE_TAG, {
-    update(cache, { data: { deleteTag } }) {
-      const tags: any = cache.readQuery({ query: GET_TAGS });
-      tags.tags = tags.tags.filter((val: any) => val.id !== deleteId);
-      cache.writeQuery({
-        query: GET_TAGS,
-        data: tags,
-      });
-    },
-  });
+	const classes = useStyles();
+	let deleteId: number = 0;
+	const { loading, error, data } = useQuery(GET_TAGS);
+	const [deleteTag] = useMutation(DELETE_TAG, {
+		update(cache, { data: { deleteTag } }) {
+			const tags: any = cache.readQuery({ query: GET_TAGS });
+			tags.tags = tags.tags.filter((val: any) => val.id !== deleteId);
+			cache.writeQuery({
+				query: GET_TAGS,
+				data: tags,
+			});
+		},
+	});
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-  const tagList = data.tags;
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error :(</p>;
+	const tagList = data.tags;
 
-  const deleteHandler = (id: number) => {
-    deleteId = id;
-    deleteTag({ variables: { id } });
-  };
+	const deleteHandler = (id: number) => {
+		deleteId = id;
+		deleteTag({ variables: { id } });
+	};
 
-  return (
-    <Paper className={styles.Paper}>
-      <Table className={styles.Table}>
-        <TableHead>
-          <TableRow>
-            <TableCell padding="default">Name</TableCell>
-            <TableCell padding="default">Description</TableCell>
-            <TableCell padding="default"></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {tagList.map((n: any) => {
-            return (
-              <TableRow key={n.id} hover>
-                <TableCell padding="default">{n.label}</TableCell>
-                <TableCell padding="default">{n.description || "-"}</TableCell>
-                <TableCell padding="default">
-                  <IconButton
-                    aria-label="Edit"
-                    color="default"
-                    onClick={() => history.push(`/tag/${n.id}/edit`)}
-                    className={classes.noVerticalPadding}
-                  >
-                    <EditIcon />
-                  </IconButton>
-                  <IconButton
-                    aria-label="Delete"
-                    color="default"
-                    onClick={() => deleteHandler(n.id)}
-                    className={classes.noVerticalPadding}
-                  >
-                    <DeleteIcon />
-                  </IconButton>
-                </TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </Paper>
-  );
+	return (
+		<Paper className={styles.Paper}>
+			<Table className={styles.Table}>
+				<TableHead>
+					<TableRow>
+						<TableCell padding="default">Name</TableCell>
+						<TableCell padding="default">Description</TableCell>
+						<TableCell padding="default"></TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{tagList.map((n: any) => {
+						return (
+							<TableRow key={n.id} hover>
+								<TableCell padding="default">{n.label}</TableCell>
+								<TableCell padding="default">{n.description || "-"}</TableCell>
+								<TableCell padding="default">
+									<IconButton
+										aria-label="Edit"
+										color="default"
+										onClick={() => history.push(`/tag/${n.id}/edit`)}
+										className={classes.noVerticalPadding}
+									>
+										<EditIcon />
+									</IconButton>
+									<IconButton
+										aria-label="Delete"
+										color="default"
+										onClick={() => deleteHandler(n.id)}
+										className={classes.noVerticalPadding}
+									>
+										<DeleteIcon />
+									</IconButton>
+								</TableCell>
+							</TableRow>
+						);
+					})}
+				</TableBody>
+			</Table>
+		</Paper>
+	);
 };
 
 const useStyles = makeStyles(() => ({
-  noVerticalPadding: {
-    padding: "0px 12px",
-  },
+	noVerticalPadding: {
+		padding: "0px 12px",
+	},
 }));
